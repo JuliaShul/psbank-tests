60ea88832b81911a17f03f4728fc7b80324c401a
shulbaeva <j.shulbaeva@cft.ru>
2021-09-14 12:18:29 +0700

    fix api tests

--- src/test/java/ru/psb/regresstests/ui/TestsMainPage.java
+++ src/test/java/ru/psb/regresstests/ui/TestsMainPage.java
@@ -1,6 +1,7 @@
 package ru.psb.regresstests.ui;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.*;
 import ru.psb.config.ProjectConfig;
@@ -16,42 +17,42 @@ import static io.qameta.allure.Allure.step;
 //@Layer("Web")
 //@Owner("shulbaeva")
 public class TestsMainPage extends TestBase {
-//    SearchFields searchFields = new SearchFields();
-// //   CommonSteps commonSteps = new CommonSteps();
-//
-//    String mainPageURL = "https://www.psbank.ru/";
+    SearchFields searchFields = new SearchFields();
+ //   CommonSteps commonSteps = new CommonSteps();
+
+    String mainPageURL = "https://www.psbank.ru/";
 
 //    @BeforeAll
 //    static void configureBaseUrl() {
 //        Configuration.baseUrl = DriverUtils.getDriverConfig.baseUrl();
 //    }
 
-//   @Story("Positive test for Search Fields on TopBar")
-//    @JiraIssues({@JiraIssue("HOM-231")})
-//    @Tags({@Tag("web"), @Tag("regress"), @Tag("UI")})
-//    @DisplayName("Check the labels on the top panel on the site")
-//    @Test
-//    public void checkTopBar() {
-//        step("Open the main page", () -> {
-//            open(mainPageURL);
-//        });
-       // commonSteps.openPage();
-//        searchFields.haveText("Офисы");
-//        searchFields.haveText("Банкоматы");
-//        searchFields.haveText("Контакты");
-//        searchFields.haveTextChapter(0,"Частным лицам");
-//        searchFields.haveTextChapter(1,"Малому бизнесу");
-//        searchFields.haveTextChapter(2,"Среднему бизнесу");
-//        searchFields.haveTextChapter(3,"Корпорациям");
-//        searchFields.haveTextChapter(4,"Гособоронзаказ");
-//        searchFields.haveTextCategory(0,"Кредиты");
-//        searchFields.haveTextCategory(1,"Вклады и накопления");
-//        searchFields.haveTextCategory(2,"Ипотека");
-//        searchFields.haveTextCategory(3,"Карты");
-//        searchFields.haveTextCategory(4,"Премиальное обслуживание");
-//        searchFields.haveTextCategory(5,"Инвестиции");
-//        searchFields.haveTextCategory(6,"Военнослужащим");
-//        searchFields.haveTextCategory(7,"Счета и переводы");
-  //  }
+   @Story("Positive test for Search Fields on TopBar")
+ //   @JiraIssues({@JiraIssue("HOM-231")})
+    @Tags({@Tag("web"), @Tag("regress"), @Tag("UI")})
+    @DisplayName("Check the labels on the top panel on the site")
+    @Test
+    public void checkTopBar() {
+        step("Open the main page", () -> {
+            Selenide.open(mainPageURL);
+        });
+      //  commonSteps.openPage();
+        searchFields.haveText("Офисы");
+        searchFields.haveText("Банкоматы");
+        searchFields.haveText("Контакты");
+        searchFields.haveTextChapter(0,"Частным лицам");
+        searchFields.haveTextChapter(1,"Малому бизнесу");
+        searchFields.haveTextChapter(2,"Среднему бизнесу");
+        searchFields.haveTextChapter(3,"Корпорациям");
+        searchFields.haveTextChapter(4,"Гособоронзаказ");
+        searchFields.haveTextCategory(0,"Кредиты");
+        searchFields.haveTextCategory(1,"Вклады и накопления");
+        searchFields.haveTextCategory(2,"Ипотека");
+        searchFields.haveTextCategory(3,"Карты");
+        searchFields.haveTextCategory(4,"Премиальное обслуживание");
+        searchFields.haveTextCategory(5,"Инвестиции");
+        searchFields.haveTextCategory(6,"Военнослужащим");
+        searchFields.haveTextCategory(7,"Счета и переводы");
+    }
 
 }
